2cb712159e6382049861c4abdeec0f3369706ef6
arpitnamdeo <narpit2@gmail.com>
2023-11-24 17:12:14 +0530

    swagger documentation

--- src/main/java/com/firstSpringBootApp/release1/Release1Application.java
+++ src/main/java/com/firstSpringBootApp/release1/Release1Application.java
@@ -2,6 +2,8 @@ package com.firstSpringBootApp.release1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
